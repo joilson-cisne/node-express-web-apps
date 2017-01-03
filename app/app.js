888d019bcbfcca2745e7a5c785a9abb1ff4a504b
Joilson Nascimento <joilsoncisne@gmail.com>
2017-01-03 17:16:52 -0200

    Add Navigation and Routing

--- app/app.js
+++ app/app.js
@@ -3,14 +3,33 @@ import express from 'express';
 let app = express();
 
 let port = process.env.PORT || 5000;
+let bookRouter = express.Router();
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
+bookRouter.route('/')
+    .get((req, res) => {
+        res.send('Hello Books!');
+    });
+
+bookRouter.route('/single')
+    .get((req, res) => {
+        res.send('Hello Single Book!');
+    });
+
+app.use('/Books', bookRouter);
+
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Hello from render', list: ['a', 'b', 'c']});
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [
+            {link: '/Books', text: 'Books'},
+            {link: '/Authors', text: 'Authors'},
+        ],
+    });
 });
 
 app.get('/books', (req, res) => {
