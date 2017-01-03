d18efd33725e58ae892693e2d2ba77775d30a3c3
Joilson Nascimento <joilsoncisne@gmail.com>
2017-01-03 17:37:50 -0200

    Add list of books (hardcoded)

--- app/app.js
+++ app/app.js
@@ -10,9 +10,37 @@ app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
+const books = [
+    {
+        title: 'This is My title 1',
+        genre: 'Fiction 1',
+        author: 'Lev Tolstoy 1',
+        read: false,
+    },
+    {
+        title: 'This is My title 2',
+        genre: 'Fiction 2',
+        author: 'Lev Tolstoy 2',
+        read: false,
+    },
+    {
+        title: 'This is My title 3',
+        genre: 'Fiction 3',
+        author: 'Lev Tolstoy 3',
+        read: false,
+    },
+];
+
 bookRouter.route('/')
     .get((req, res) => {
-        res.send('Hello Books!');
+        res.render('books', {
+            title: 'Books',
+            nav: [
+                {link: '/Books', text: 'Books'},
+                {link: '/Authors', text: 'Authors'},
+            ],
+            books: books,
+        });
     });
 
 bookRouter.route('/single')
