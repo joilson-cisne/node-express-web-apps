373a9d3dbb4393e0b305956f44374c4784ca53be
Joilson Nascimento <joilsoncisne@gmail.com>
2017-01-02 23:31:59 -0200

    Change Jade for Handlebars

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 let app = express();
 
@@ -6,10 +7,12 @@ let port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.set('views', './src/views');
-app.set('view engine', 'jade');
+
+app.engine('.hbs', handlebars({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 app.get('/', (req, res) => {
-    res.render('index', {list: ['a', 'b', 'c']});
+    res.render('index', {title: 'Hello from render', list: ['a', 'b', 'c']});
 });
 
 app.get('/books', (req, res) => {
